dcfdf59ec0ae09c5a3e3a857bc22b70420578fff
cj.park <sisyphus9402@gmail.com>
2023-06-16 11:46:00 +0900

    New branch: test-1

--- web_app/app/views/main_view.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-from werkzeug.utils import redirect
-
-bp = Blueprint("main", __name__, url_prefix="/")
-
-@bp.route("/")
-def index():
-    return render_template("page/home/index.html")
